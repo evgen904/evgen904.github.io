8a2f289dacc68e795908c4574f61d3a00a6e4135
Evgen904 <evgen904@gmail.com>
2022-02-11 12:41:56 +0400

    add new build

---
+++ precache-manifest.e305af5114de7303a9680ff4f695bb3c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84e8e4a2547a1cd57ca",
+    "url": "css/chunk-1ac45164.d86d6d71.css"
+  },
+  {
+    "revision": "ccc9867ccedb3731c39f",
+    "url": "css/chunk-2cf3d68c.ced4f8d7.css"
+  },
+  {
+    "revision": "1810876941e3f7973bb1",
+    "url": "css/chunk-5408c059.cefb1205.css"
+  },
+  {
+    "revision": "f7a66f18f1981617725e",
+    "url": "header-vue.css"
+  },
+  {
+    "revision": "f7a66f18f1981617725e",
+    "url": "header-vue.min.js"
+  },
+  {
+    "revision": "bbc00a9be1a414a3b0fe8d306dded974",
+    "url": "img/eye.bbc00a9b.svg"
+  },
+  {
+    "revision": "e116f20c0f72b968188f4de1ba0b1608",
+    "url": "index.html"
+  },
+  {
+    "revision": "e84e8e4a2547a1cd57ca",
+    "url": "js/chunk-1ac45164.86217c22.js"
+  },
+  {
+    "revision": "ccc9867ccedb3731c39f",
+    "url": "js/chunk-2cf3d68c.c44af397.js"
+  },
+  {
+    "revision": "5181d5858e92207fbaa3",
+    "url": "js/chunk-2d0e95df.0eaf2934.js"
+  },
+  {
+    "revision": "1810876941e3f7973bb1",
+    "url": "js/chunk-5408c059.ef0d51f0.js"
+  },
+  {
+    "revision": "f1b2263da05893fb1bfbddfc42a2d9a3",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
